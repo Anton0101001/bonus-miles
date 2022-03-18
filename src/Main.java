1bc375bc82461912de0d37ffee79e6c8a7f32d46
anton shabanov <89005421555@mail.ru>
2022-03-18 13:27:28 +0300

    botus

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
+
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("ЛЕТАТЬ С НАМИ ВЫГОДНО!");
+
+
+
+                int ticket = 8_000;
+                int rubles = 20;
+
+               int bonus = (ticket / rubles);
+
+                System.out.println("Поздравляем, Вам начислено: " + bonus + " бонусных миль.");
 
 
 
 
 
-    }
+            }
+        }
 
 
 
 
-}
+
+
+
+
+
+
