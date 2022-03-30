8efab1847d6e1ff0afb15ae2669acad528c8faf3
anton shabanov <89005421555@mail.ru>
2022-03-30 09:55:44 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
 
-        public class Main {
-            public static void main(String[] args) {
-                System.out.println("ЛЕТАТЬ С НАМИ ВЫГОДНО!");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ЛЕТАТЬ С НАМИ ВЫГОДНО!");
 
 
+        int ticket = 8_000;
+        int rubles = 20;
 
-                int ticket = 8_000;
-                int rubles = 20;
+        int bonus = (ticket / rubles);
 
-               int bonus = (ticket / rubles);
-
-                System.out.println("Поздравляем, Вам начислено: " + bonus + " бонусных миль.");
-
+        System.out.println("Поздравляем, Вам начислено: " + bonus + " бонусных миль.");
 
 
-
-
-            }
-        }
+    }
+}
 
 
 
